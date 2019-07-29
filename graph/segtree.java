033ca10d206ca7f3a6c1b70540e4cb4dbc5c5b8a
Daniel Kim <chipperday@gmail.com>
2019-07-28 23:57:19 -0700

    segtree updated

--- graph/segtree.java
+++ graph/segtree.java
@@ -6,17 +6,44 @@ import java.util.Arrays;
 
 class segtree {
   int[] tree;
+  int n; // size of the original array
 
-  segtree(int[] ar) {
-    int n = ar.length;
+  segtree(int n) {
+    this.n = n;
     int height = (int) Math.ceil(Math.log(n) / Math.log(2));
     int max_size = (int) Math.pow(2, height + 1) - 1;
     tree = new int[max_size];
-    
-    makeSegTree(ar, 0, n-1, 0);
-    System.out.println(Arrays.toString(tree));
   }
 
+  segtree(int[] ar) {
+    this(ar.length);
+    makeSegTree(ar, 0, n - 1, 0);
+  }
+
+  // v = new value
+  // i = index
+  public void update(int i, int v) {
+    update(i, v, 0, n - 1, 0);
+  }
+
+  private void update(int index, int value, int start, int end, int i) {
+    System.out.format("%d-%d %d (%d)\n", start, end, i, tree[i]);
+
+    if (start > index || end < index) {
+      System.out.println("end");
+      return;
+    }
+
+    int mid = (start + end) / 2;
+    tree[i] = Math.min(tree[i], value);
+
+    if (start != end) {
+      update(index, value, start, mid, i * 2 + 1);
+      update(index, value, mid + 1, end, i * 2 + 2);
+    }
+  }
+
+  // [a, b]
   int makeSegTree(int ar[], int a, int b, int i) {
     // exit condition
     if (a == b)
@@ -24,12 +51,15 @@ class segtree {
 
     // recursion
     int mid = a + (b - a) / 2;
-    return tree[i] = Math.min(
-      makeSegTree(ar, a, mid, i * 2 + 1), 
-      makeSegTree(ar, mid+1, b, i * 2 + 2));
+    return tree[i] = Math.min(makeSegTree(ar, a, mid, i * 2 + 1), makeSegTree(ar, mid + 1, b, i * 2 + 2));
   }
 
-  int queryMin(int a, int b, int aa, int bb, int ti) {
+  public int queryMin(int a, int b) {
+    return queryMin(a, b, 0, n - 1, 0);
+  }
+
+  // [a, b], [aa, bb]
+  private int queryMin(int a, int b, int aa, int bb, int ti) {
     if (bb < a || aa > b) {
       return Integer.MAX_VALUE;
     }
@@ -39,33 +69,25 @@ class segtree {
 
     int mid = aa + (bb - aa) / 2;
 
-    return Math.min(
-      queryMin(a, b, aa, mid, ti * 2 + 1),
-      queryMin(a, b, mid+1, bb, ti * 2 + 2)
-    );
-  }
-
-  int querySum(int a, int b, int aa, int bb, int ti) {
-    if (bb < a || aa > b) {
-      return 0;
-    }
-    if (a <= aa && bb <= b) {
-      return tree[ti];
-    }
-
-    int mid = aa + (bb - aa) / 2;
-
-    return 
-      querySum(a, b, aa, mid, ti * 2 + 1) +
-      querySum(a, b, mid+1, bb, ti * 2 + 2);
+    return Math.min(queryMin(a, b, aa, mid, ti * 2 + 1), queryMin(a, b, mid + 1, bb, ti * 2 + 2));
   }
 
   public static void main(String[] args) {
     int[] ar = { 2, 5, 1, 4, 9, 3 };
 
     segtree s = new segtree(ar);
-    int res = s.querySum(2, 5, 0, ar.length-1, 0);
-    System.out.println(res);
+    System.out.println(Arrays.toString(s.tree));
+
+    for (int i = 0; i < ar.length - 2; i++) {
+      System.out.println(ar[i] + "~" + ar[i + 2] + "=" + s.queryMin(i, i + 2));
+    }
+
+    s.update(4, 2);
+    System.out.println(Arrays.toString(s.tree));
+
+    for (int i = 0; i < ar.length - 2; i++) {
+      System.out.println(ar[i] + "~" + ar[i + 2] + "=" + s.queryMin(i, i + 2));
+    }
   }
 
 }
---
+++ graph/segtree_sum.java
@@ -0,0 +1,67 @@
+class segtree {
+    int[] tree;
+    int n; // size of the original array
+
+    segtree(int n) {
+        this.n = n;
+        int height = (int) Math.ceil(Math.log(n) / Math.log(2));
+        int max_size = (int) Math.pow(2, height + 1) - 1;
+        tree = new int[max_size];
+    }
+
+    segtree(int[] ar) {
+        this(ar.length);
+        makeSegTree(ar, 0, n - 1, 0);
+    }
+
+    // v = new value
+    // i = index
+    public void update(int i, int diff) {
+        update(i, diff, 0, n - 1, 0);
+    }
+
+    private void update(int index, int diff, int start, int end, int i) {
+
+        if (start > index || end < index) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        tree[i] += diff;
+
+        if (start != end) {
+            update(index, diff, start, mid, i * 2 + 1);
+            update(index, diff, mid + 1, end, i * 2 + 2);
+        }
+    }
+
+    // [a, b]
+    int makeSegTree(int ar[], int a, int b, int i) {
+        // exit condition
+        if (a == b)
+            return tree[i] = ar[a];
+
+        // recursion
+        int mid = a + (b - a) / 2;
+        return tree[i] = makeSegTree(ar, a, mid, i * 2 + 1) + makeSegTree(ar, mid + 1, b, i * 2 + 2);
+    }
+
+    public int query(int a, int b) {
+        return query(a, b, 0, n - 1, 0);
+    }
+
+    // [a, b], [aa, bb]
+    private int query(int a, int b, int aa, int bb, int ti) {
+        if (bb < a || aa > b) {
+            return 0;
+        }
+        if (a <= aa && bb <= b) {
+            return tree[ti];
+        }
+
+        int mid = aa + (bb - aa) / 2;
+
+        return query(a, b, aa, mid, ti * 2 + 1) + query(a, b, mid + 1, bb, ti * 2 + 2);
+    }
+
+}
