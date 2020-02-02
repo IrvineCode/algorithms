3b2b2373c039e322e043dbd2a5aae04136c1723d
Daniel Kim <chipperday@gmail.com>
2020-02-02 00:24:43 -0800

    dynamic seg tree

--- gold/dsegtree.java
+++ gold/dsegtree.java
@@ -1,9 +1,11 @@
 class dsegtree {
   public static void main(String[] args) {
     int[] ar = { 2, 5, 1, 4, 9, 3 };
+    // 2
 
     node seg = new node(0, 6);
     for (int i = 0; i < ar.length; i++) {
+      System.out.println("adding " + ar[i]);
       seg.update(i, ar[i]);
     }
 
@@ -13,7 +15,7 @@ class dsegtree {
 
     seg.update(4, 2);
     System.out.println("-------");
-    
+
     for (int i = 0; i < ar.length - 2; i++) {
       System.out.println(ar[i] + "~" + ar[i + 2] + "=" + seg.getMax(i, i + 2));
     }
@@ -31,17 +33,27 @@ class node {
   }
 
   void extend() {
+    if (lv == rv)
+      return;
     if (left == null) {
-      int m = (lv+rv)/2;
+      int m = (lv + rv) / 2;
       left = new node(lv, m);
-      right = new node(m+1, rv);
+      right = new node(m + 1, rv);
     }
   }
 
   int getMax(int l, int r) {
-    if (r < lv || rv < l) return Integer.MIN_VALUE;
-    if (l <= lv && rv <= r) return max;
-    extend();
+    if (r < lv || rv < l) {
+      return 0;
+    }
+    if (l <= lv && rv <= r) {
+      return max;
+    }
+
+    // extend();
+    if (left == null)
+      return 0;
+
     return Math.max(left.getMax(l, r), right.getMax(l, r));
   }
 
@@ -51,10 +63,18 @@ class node {
       return;
     }
     extend();
+
     if (p <= left.rv)
       left.update(p, newVal);
     else
       right.update(p, newVal);
+
+    max = Math.max(left.max, right.max);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("[ %d ~ %d ]", lv, rv);
   }
 }
 
