ecdf9478a5b99959f7776aef38abc294ca9992d0
Daniel Kim <chipperday@gmail.com>
2020-04-06 12:07:55 -0700

    Update segtree.java

--- gold/segtree.java
+++ gold/segtree.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 //[1, 1, 3, 2, 1, 4, 3, 2, 5, x, x, 4, 9, x, x]
 //[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-class segtree {
+class minSeg {
   int[] tree;
   int n; // size of the original array
 
-  segtree(int n) {
+  minSeg(int n) {
     this.n = n;
     int height = (int) Math.ceil(Math.log(n) / Math.log(2));
     int max_size = (int) Math.pow(2, height + 1) - 1;
     tree = new int[max_size];
+    Arrays.fill(tree, Integer.MAX_VALUE);
   }
 
-  segtree(int[] ar) {
+  minSeg(int[] ar) {
     this(ar.length);
     makeSegTree(ar, 0, n - 1, 0);
   }
@@ -27,10 +28,7 @@ class segtree {
   }
 
   private void update(int index, int value, int start, int end, int i) {
-    System.out.format("%d-%d %d (%d)\n", start, end, i, tree[i]);
-
     if (start > index || end < index) {
-      System.out.println("end");
       return;
     }
 
@@ -71,23 +69,70 @@ class segtree {
 
     return Math.min(queryMin(a, b, aa, mid, ti * 2 + 1), queryMin(a, b, mid + 1, bb, ti * 2 + 2));
   }
+}
 
-  public static void main(String[] args) {
-    int[] ar = { 2, 5, 1, 4, 9, 3 };
+class maxSeg {
+  int[] tree;
+  int n; // size of the original array
 
-    segtree s = new segtree(ar);
-    System.out.println(Arrays.toString(s.tree));
+  maxSeg(int n) {
+    this.n = n;
+    int height = (int) Math.ceil(Math.log(n) / Math.log(2));
+    int max_size = (int) Math.pow(2, height + 1) - 1;
+    tree = new int[max_size];
+  }
 
-    for (int i = 0; i < ar.length - 2; i++) {
-      System.out.println(ar[i] + "~" + ar[i + 2] + "=" + s.queryMin(i, i + 2));
+  maxSeg(int[] ar) {
+    this(ar.length);
+    makeSegTree(ar, 0, n - 1, 0);
+  }
+
+  // v = new value
+  // i = index
+  public void update(int i, int v) {
+    update(i, v, 0, n - 1, 0);
+  }
+
+  private void update(int index, int value, int start, int end, int i) {
+    if (start > index || end < index) {
+      return;
     }
 
-    s.update(4, 2);
-    System.out.println(Arrays.toString(s.tree));
+    int mid = (start + end) / 2;
+    tree[i] = Math.max(tree[i], value);
 
-    for (int i = 0; i < ar.length - 2; i++) {
-      System.out.println(ar[i] + "~" + ar[i + 2] + "=" + s.queryMin(i, i + 2));
+    if (start != end) {
+      update(index, value, start, mid, i * 2 + 1);
+      update(index, value, mid + 1, end, i * 2 + 2);
     }
   }
 
+  // [a, b]
+  int makeSegTree(int ar[], int a, int b, int i) {
+    // exit condition
+    if (a == b)
+      return tree[i] = ar[a];
+
+    // recursion
+    int mid = a + (b - a) / 2;
+    return tree[i] = Math.max(makeSegTree(ar, a, mid, i * 2 + 1), makeSegTree(ar, mid + 1, b, i * 2 + 2));
+  }
+
+  public int queryMax(int a, int b) {
+    return queryMax(a, b, 0, n - 1, 0);
+  }
+
+  // [a, b], [aa, bb]
+  private int queryMax(int a, int b, int aa, int bb, int ti) {
+    if (bb < a || aa > b) {
+      return -1;
+    }
+    if (a <= aa && bb <= b) {
+      return tree[ti];
+    }
+
+    int mid = aa + (bb - aa) / 2;
+
+    return Math.max(queryMax(a, b, aa, mid, ti * 2 + 1), queryMax(a, b, mid + 1, bb, ti * 2 + 2));
+  }
 }
