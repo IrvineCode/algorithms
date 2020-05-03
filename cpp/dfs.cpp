0295bc9a82d433a2ce957af2b33ba2e0257a64e0
Daniel Kim <chipperday@gmail.com>
2020-05-02 23:42:59 -0700

    Update dfs.cpp

--- cpp/dfs.cpp
+++ cpp/dfs.cpp
@@ -1,40 +1,45 @@
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 list<int> *adj;
 bool *visited;
 
-void dfs(int u) {
+void dfs(int u)
+{
     printf("--> %d ", u);
-    for(int v : adj[u]) {
-        if (!visited[v]) {
+    for (int v : adj[u])
+    {
+        if (!visited[v])
+        {
             visited[v] = true;
             dfs(v);
         }
     }
-} 
+}
 
-int main() {
-		ios_base::sync_with_stdio(false);
+int main()
+{
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     // input processing
     int V, E;
     cin >> V;
     cin >> E;
-    
+
     adj = new list<int>[V];
     visited = new bool[V];
-    fill(visited, visited+V, false);
-    
-    for(int i = 0; i < E; i++) {
+    fill(visited, visited + V, false);
+
+    for (int i = 0; i < E; i++)
+    {
         int a, b;
         cin >> a;
         cin >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    
+
     visited[0] = true;
     dfs(0);
 }
